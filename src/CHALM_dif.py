90eb32c944e23f9cc208fa390f27af71a8a491fb
Jianfeng Xu <jianfengxu@Jianfengs-MacBook-Pro-2.local>
2019-07-27 19:34:26 -0700

    add license_02

--- src/CHALM_dif.py
+++ src/CHALM_dif.py
@@ -1,4 +1,22 @@
 #! /usr/bin/env python
+
+# Copyright 2018 Terry Jones, Wei Li
+
+# This file is part of CHALM.
+
+# CHALM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# CHALM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with CHALM.  If not, see <https://www.gnu.org/licenses/>.
+
 import sys, time, os
 import pandas as pd
 import argparse
